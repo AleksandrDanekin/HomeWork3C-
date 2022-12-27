0d98327632cd1e6e7b2d45aacbf958dd9463ad75
Aleksandr Danekin <danekinas_78@mail.ru>
2022-12-27 14:55:47 +0400

    Решение д/з к 3 семинару по С#

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 1. Написать программу, которая принимает на вход пятизначное число и проверяет, является ли оно полиндромом.
+
+// System.Console.WriteLine("Введите пятизначное число: ");
+// string number = Console.ReadLine();
+// if(number.Length < 5 || number.Length > 5)
+// {
+//     System.Console.WriteLine("Ошибка!!! Число должно быть пятизначным");
+// }
+// else if(number[0] == number[4] && number[1] == number[3])
+// {
+//     System.Console.WriteLine($"Число {number} является полиндромом");
+// }
+// else 
+// {
+//     System.Console.WriteLine($"Число {number} НЕ является полиндромом");
+// }
+
+
+
+
+//Задача 2. Написать программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.Write("Введите координату X первой точки: ");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Y первой точки: ");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Z первой точки: ");
+// int Z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату X второй точки: ");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Y второй точки: ");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату Z второй точки: ");
+// int Z2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Round(Math.Sqrt(Math.Pow((X1 - X2), 2) + Math.Pow((Y1-Y2), 2) + Math.Pow((Z1 - Z2), 2)), 2);
+// Console.Write($"Расстояние между двумя точками: {result}");
+
+
+
+
+//Задача 3. Написать программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// for (int Z = 1; Z <= number; Z++)
+// {
+//     System.Console.WriteLine(Math.Pow(Z, 3));
+// }
+
+
+
+
+
+
+
+
+
+
